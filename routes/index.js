7a2bf6b64113128b8fb520cddb85a18801c1dbd7
전수림 <gds0813@naver.com>
2019-05-26 19:18:10 +0900

    심리검사 페이지 진행중

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ var usersRouter = require('./users');
 var loginRouter = require('./login');
 var login2Router = require('./login2');
 var signRouter = require('./sign');
+var psy_testRouter = require('./psy_test');
 
 router.use("/",mainRouter);
 router.use("/api",api);
@@ -14,5 +15,6 @@ router.use("/users",usersRouter);
 router.use("/login",loginRouter);
 router.use("/login2",login2Router);
 router.use("/sign",signRouter);
+router.use("/psy_test",psy_testRouter);
 
 module.exports = router;
