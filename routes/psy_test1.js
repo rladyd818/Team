efaca14a604b8394966b34428ccfc8c2c220fb05
전수림 <gds0813@naver.com>
2019-05-28 17:48:06 +0900

    main page css, psy_test  html

--- routes/psy_test1.js
+++ routes/psy_test1.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("psy_test진입");
-  res.render('psy_test');
+  console.log("psy_test1진입");
+  res.render('psy_test1');
 });
 
 module.exports = router;
