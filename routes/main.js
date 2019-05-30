7955cbb8a66e1865911e61e7da58d1d2b45cc665
kimyong <rladyd818@naver.com>
2019-05-30 15:04:48 +0900

    임시커밋

--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var router = express.Router();
-//var axios = require('axios');
-//var kakao = require('./javascripts/kakaoSDK');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log("mainpage진입" );
+router.get('/', function(req, res, next) {  
   res.render('main', { name: 'LOGIN' } );
 });
 
